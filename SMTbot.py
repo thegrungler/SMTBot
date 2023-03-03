51ac3d23f1fe301dd99e27112bfb079d468b2960
Luka Lane <126537943+Boncers@users.noreply.github.com>
2023-03-03 13:07:47 -0700

    SMTbot.py
    Main!

---
+++ SMTbot.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
