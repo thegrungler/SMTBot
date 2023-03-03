03a65fdbbb6ed858da8533baa77c60570c185b92
Luka Lane <126537943+Boncers@users.noreply.github.com>
2023-03-03 13:41:47 -0700

    Updated SMTbot.py & added SMT.py
    SMT.py is to store classes and function. SMTbot.py is the same as "main()" for the bot. Currently we are only storing placeholders and testing elements

---
+++ SMT.py
@@ -0,0 +1,15 @@
+class demon:
+    def __init__(demon, name, lvl, move1, move2, move3):
+        demon.name = name
+        demon.lvl = lvl
+        demon.move1 = move1
+        demon.move2 = move2
+        demon.move3 = move3
+    
+    def intro(self):
+        print("Hello! My name is "+ self.name)
+
+
+def dmgCalc(atk, EnDef):
+    print("damage!")
+    return
--- SMTbot.py
+++ SMTbot.py
@@ -1,12 +1,16 @@
 import discord
 from discord.ext import commands
 import random
+import SMT
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+Pixie = SMT.demon("Pixie", 1, "Dia", "Zio", "Garu")
+Pixie.intro()
+print("\n", SMT.dmgCalc(1,1))
+#@client.event
+#async def on_ready():
+#    print(f'We have logged in as {client.user}')
