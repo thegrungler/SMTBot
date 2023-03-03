7c0379705ab273fc4a1fff2d84399872afafcc99
thegrungler <126526811+thegrungler@users.noreply.github.com>
2023-03-03 16:04:22 -0700

    added basis for demon.levelup()

--- SMT.py
+++ SMT.py
@@ -1,18 +1,40 @@
 import random
 
 class demon:
-    def __init__(demon, name, lvl, moves: list, stats: list, growths: list):
+    def __init__(demon, name: str, lvl: int, skills: list, learnset: list, stats: list, growths: list):
         demon.name = name
         demon.lvl = lvl
 #        demon.location = location
 #        demon.chance = chance
-        demon.moves = moves
+        demon.moves = skills
+        demon.learnset = learnset
         demon.stats = stats
         demon.growths = growths
     
     def intro(self):
         message = "Hello! My name is "+ self.name
         return message
+    
+    def levelup(self):
+        i = 0
+        while i < 3:
+            temp = random.randint(0, 100)
+            chance0 = self.growths[0]
+            chance1 = chance0+self.growths[1]
+            chance2 = chance1+self.growths[2]
+            chance3 = chance2+self.growths[3]
+            chance4 = chance3+self.growths[4]
+            if temp < chance0:
+                self.stats[0]+=1
+            elif temp > chance0 and temp < chance1:
+                self.stats[1]+=1
+            elif temp > chance1 and temp < chance2:
+                self.stats[2]+=1
+            elif temp > chance2 and temp < chance3:
+                self.stats[3]+=1
+            elif temp > chance3 and temp < chance4:
+                self.stats[4]+=1
+        
 
 class player:
     def __init__(player, ID, lvl, storage):
