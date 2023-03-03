abd5899e2d11841b26b3d35cd2870909e523fe72
Luka Lane <126537943+Boncers@users.noreply.github.com>
2023-03-03 14:33:49 -0700

    Updates SMTbot.py & SMT.py
    Added first few commands

--- SMT.py
+++ SMT.py
@@ -1,15 +1,54 @@
+import random
+
 class demon:
     def __init__(demon, name, lvl, move1, move2, move3):
         demon.name = name
         demon.lvl = lvl
+#        demon.location = location
+#        demon.chance = chance
         demon.move1 = move1
         demon.move2 = move2
         demon.move3 = move3
     
     def intro(self):
-        print("Hello! My name is "+ self.name)
+        message = "Hello! My name is "+ self.name
+        return message
 
+class player:
+    def __init__(player, ID, lvl, storage):
+        player.ID = ID
+        player.lvl = lvl
+        player.storage = {}
+
+def encountering(lvl):
+    if lvl == 1:
+        Pixie = demon("Pixie", 1, "Dia", "Zio", " ")
+        return Pixie
+
+    if (lvl > 1) and (lvl < 5):
+        chance = random.randint(1,3)
+        if (chance < 3):
+            Slime = demon("Slime", 1, "Lunge", "Dustoma", "Poisma")
+            return Slime
+        else:
+            PyroJack = demon("Pyro Jack", 3, "Agi", "Sukunda", "Rarukaja")
+            return PyroJack
+
+def negotiation(lvl):
+
+    return
+
+def fusion(type1, lvl1, type2, lvl2):
+
+    return
 
 def dmgCalc(atk, EnDef):
     print("damage!")
     return
+
+#Checks if a given value is in a given dictionary
+def checkKey(dict, key):
+  if key in dict.keys():
+    return True
+  else:
+    return False
--- SMTbot.py
+++ SMTbot.py
@@ -1,16 +1,42 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 import random
 import SMT
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-Pixie = SMT.demon("Pixie", 1, "Dia", "Zio", "Garu")
-Pixie.intro()
-print("\n", SMT.dmgCalc(1,1))
-#@client.event
-#async def on_ready():
-#    print(f'We have logged in as {client.user}')
+PlayerList = {}
+
+@tree.command(name="NewPlayer", description="You can start finding demons!")
+async def NewPlayer(interaction):
+    if SMT.CheckKey() == True:
+        return
+    PlayerList[interaction.user.id] = SMT.player(interaction.user.id, 1, [])
+    await interaction.response.send_message("You have been summoned!")
+    
+
+
+@tree.command(name="Encounter", description="Find a demon!")
+async def encounter(interaction):
+    encounter = SMT.encountering(1)
+    message = encounter.intro()
+    await interaction.response.send_message(message)
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  await client.change_presence(activity=discord.Game(name="with your balls"))
+  print("syncing commands")
+  await tree.sync()
+  print("commands are Ready!")
+client.run(DISCORD_TOKEN)
